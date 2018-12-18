fabf821916b0971f08ced82c7eb9e04f9ef4ca0d
unknown <r.angelov33@gmail.com>
2018-12-19 00:22:07 +0200

    Added deploy script to package.json

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<quizland>/<dev-frontend>.git master:gh-pages
+git push -f https://github.com/quizland/dev-frontend.git master:gh-pages
 
 cd -
